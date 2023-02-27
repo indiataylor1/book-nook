d0fd149d40b4c698134d53415cb5367c413ac4bd
C M Santosh Kumar <pagadala.kalyani@gmail.com>
2023-02-27 20:23:07 +0000

    book author details added in the routes

--- controllers/API/shop-routes.js
+++ controllers/API/shop-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const { shop } = require('../../models');
+const { Shop } = require('../../models');
+const { Author } = require('../..models');
+const { Book } = require('../..models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
@@ -7,6 +9,12 @@ router.post('/', withAuth, async (req, res) => {
         const newBook = await Book.create({
             ...req.body,
             user_id: req.session.user_id,
+            id: req.session.id,
+            book_name: req.session.book_name,
+            price: req.session.price,
+            genre_id: req.session.genre_id,
+            id: req.session.id,
+            author_name: req.session.author_name
         });
 
         res.status(200).json(newBook);
