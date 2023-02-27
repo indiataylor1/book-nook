9d70df07f8c2120259453d9eb743ef312c1ba43b
Sakariya Adam <niyyex@live.co.uk>
2023-02-27 19:58:04 +0000

    seed and models files have been created

---
+++ models/Book.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Book extends Model { }
+
+Book.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        book_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+            validate: {
+                isDecimal: true,
+            },
+        },
+        genre_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "genre",
+                key: "id",
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'book',
+    }
+);
+
+module.exports = Book;
