5d4b9664ddace1cb30acffde115574f610b36700
joseph <josephcotterell@hotmail.co.uk>
2023-03-01 20:41:42 +0000

    added shop.js file

--- models/wishlist.js
+++ models/wishlist.js
@@ -38,5 +38,38 @@ function printView() {
   for (var i = 0; i < myList.length; i++) {
     wishList.innerHTML += "<li>" + myList[i] + "</li>";
   }
+
+var event
+
+/// button actions
+  Ul.addEventListener("click", (event)) => {
+    if (event.target.tagName === 'BUTTON'){
+      const button = event.target;
+      const li = button.parentNode;
+      const ul = li.parentNode;
+      if(button.textContent === 'remove'){
+        ul.removeChild(li);
+      } else if (button.textContent === 'edit'){
+        const span = li.firstElementChild;
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.value = span.textContent;
+        li.insertBefore(input, span)
+        li.removeChild(span);
+        button.textContent = 'save';
+      }else if (button.textContent === 'save'){
+        const input = li.firstElementChild;
+        const span = document.createElement('span')
+        span.textContent = input.value;
+        li.insertBefore(span, input);
+        li.removeChild(input);
+        button.textContent  = 'edit';
+
+
+        }
+
+      }
+    }
+  }
   //window.print();
-}
+
