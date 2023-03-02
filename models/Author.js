ab4fea17003f15913e74c467e6d6c97e2e7ddd48
Sakariya Adam <niyyex@live.co.uk>
2023-03-02 12:41:47 +0000

    correction of file location

--- models/Author.js
+++ models/Author.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../config/connection.js');
+const sequelize = require('../home/config/connection');
 
 class Author extends Model { }
 
